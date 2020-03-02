85ccfc270614e62cc31d24f0c6b7dfa9b4721807
CoderKevL <Kevin.Lunden@gmail.com>
2020-03-01 17:38:10 -0800

    Tests done

--- binsearch_collection.h
+++ binsearch_collection.h
@@ -1,10 +1,10 @@
 //----------------------------------------------------------------------
-// Author:
+// Author: Kevin Lunden
 // Course: CPSC 223, Spring 2020
 // Assign: 5
 // File:   binsearch_collection.h
 //
-// TODO: Description here
+// TODO: More practice with vectors. Practice using binary search.
 //----------------------------------------------------------------------
 
 #ifndef BINSEARCH_COLLECTION_H
@@ -13,7 +13,7 @@
 #include <vector>
 #include <algorithm>            
 #include "collection.h"
-
+using namespace std;
 
 template<typename K, typename V>
 class BinSearchCollection : public Collection<K,V>
@@ -51,7 +51,116 @@ private:
 
 };
 
+template <typename K, typename V>
+bool BinSearchCollection<K,V>::binsearch(const K& key, int& index) const
+{
+   pair<K,V> p;
+   int bottom = 0;
+   int top = kv_list.size()-1;
+   int middle;
 
-// TODO: Implement functions here ...
+   while (bottom <= top)
+   {
+    middle = (top+bottom)/2;
+    p = kv_list.at(middle);
+
+    if (key == p.first)
+    {
+     index = middle;
+     return true;
+    }       
+    else if (key > p.first)
+     bottom = middle+1;
+    else if (key < p.first)
+     top = middle-1;
+   }        
+  
+   if (kv_list.size() != 0)
+    index = bottom;
+   else
+    index = 0;
+
+   return false;
+}
+
+// add a new key-value pair into the collection
+template <typename K, typename V>
+void BinSearchCollection <K,V>::add(const K& a_key, const V& a_val)
+{
+ int index;
+ pair<K,V> tmp;
+ tmp.first = a_key;
+ tmp.second = a_val;
+
+ if (binsearch(a_key, index) == false)
+  kv_list.insert(kv_list.begin()+index, tmp);
+}
+
+// remove a key-value pair from the collectiona
+template <typename K, typename V>
+void BinSearchCollection <K,V>::remove(const K& a_key)
+{
+ int index;
+ if (binsearch(a_key, index) == true)
+  kv_list.erase(kv_list.begin()+index);
+}
+
+// find and return the value associated with the key
+template <typename K, typename V>
+bool BinSearchCollection <K,V>::find(const K& search_key, V& the_val) const
+{
+ int index;
+ pair<K,V> tmp;
+ if (binsearch(search_key, index) == true)
+ {
+  tmp = kv_list.at(index);
+  the_val = tmp.second;
+  return true;
+ }
+
+ return false;
+}
+
+// find and return the values with keys >= to k1 and <= to k2 
+template <typename K, typename V>
+void BinSearchCollection <K,V>::find(const K& k1, const K& k2, std::vector<V>& vals) const
+{
+ int index;
+ binsearch(k1, index);
+ pair<K,V> tmp = kv_list[index];
+
+ while (tmp.first <= k2 && index < kv_list.size()) 
+ {
+  vals.push_back(tmp.second);
+  index++;
+  if (index < kv_list.size())
+   tmp = kv_list[index];
+ }
+}
+
+// return all of the keys in the collection 
+template <typename K, typename V>
+void BinSearchCollection <K,V>::keys(std::vector<K>& all_keys) const
+{
+ if (kv_list.size() > 0)
+ {
+  for (pair<K,V> tmp : kv_list)
+   all_keys.push_back(tmp.first);
+ }
+}
+
+// return all of the keys in ascending (sorted) order
+template <typename K, typename V>
+void BinSearchCollection <K,V>::sort(std::vector<K>& all_keys_sorted) const
+{
+ this->keys(all_keys_sorted);
+}
+
+// return the number of key-value pairs in the collection
+template <typename K, typename V>
+int BinSearchCollection <K,V>::size() const
+{
+ return kv_list.size();
+}
 
 #endif
--- hw5_test.cpp
+++ hw5_test.cpp
@@ -1,10 +1,10 @@
 //----------------------------------------------------------------------
-// Author:
+// Author: Kevin Lunden
 // Course: CPSC 223, Spring 2020
 // Assign: 5
 // File:   hw5_test.cpp
 //
-// TODO: Description here
+// TODO: Tests BinSearchCollection.h
 //----------------------------------------------------------------------
 
 
@@ -63,7 +63,7 @@ TEST(BasicListTest, SimpleRemoveElems) {
   ASSERT_EQ(false, c.find("c", v));  
   c.remove("d");
   ASSERT_EQ(0, c.size());
-  ASSERT_EQ(false, c.find("c", v));  
+  ASSERT_EQ(false, c.find("d", v));  
 }
 
 // Test 4
@@ -103,8 +103,115 @@ TEST(BasicListTest, SimpleSort) {
     ASSERT_LE(sorted_ks[i], sorted_ks[i+1]);
 }
 
+// Test 6
+// Tests more additions
+TEST(BasicListTest, CorrectSizeAdvanced) {
+ BinSearchCollection<string,double> c;
+  ASSERT_EQ(0, c.size());
+  c.add("b", 10.0);
+  ASSERT_EQ(1, c.size());
+  c.add("a", 20.0);
+  ASSERT_EQ(2, c.size());
+  c.add("c", 20.0);
+  ASSERT_EQ(3, c.size());
+  c.add("d", 30.0);
+  c.add("e", 40.0);
+  ASSERT_EQ(5, c.size());
+}
 
-// TODO: ... additional tests ...
+// Test 7
+// Tests multiple additions with same values
+TEST(BasicListTest,AdvancedFind) {
+ BinSearchCollection<string,double> c;
+  double v;
+  ASSERT_EQ(false, c.find("b", v));
+  c.add("b", 10.0);
+  ASSERT_EQ(true, c.find("b", v));
+  ASSERT_EQ(10.0, v);
+  ASSERT_EQ(false, c.find("a", v));
+  c.add("a", 20.0);
+  ASSERT_EQ(true, c.find("a", v));
+  ASSERT_EQ(20.0, v);
+  c.add("c", 20.0);
+  ASSERT_EQ(true, c.find("c", v));
+  ASSERT_EQ(20.0, v);
+}
+
+// Test 8
+// Tests to see if correct values have been removed
+TEST(BasicListTest, AdvancedRemoveElems) {
+  BinSearchCollection<string,int> c;
+  c.add("b", 10);
+  c.add("a", 20);
+  c.add("d", 30);
+  c.add("c", 30);
+  ASSERT_EQ(4, c.size());
+  int v;
+  c.remove("a");
+  ASSERT_EQ(3, c.size());
+  ASSERT_EQ(true, c.find("b", v));
+  ASSERT_EQ(true, c.find("c", v));
+  ASSERT_EQ(true, c.find("d", v));
+  ASSERT_EQ(false, c.find("a", v));
+  c.remove("b");
+  ASSERT_EQ(2, c.size());
+  ASSERT_EQ(false, c.find("b", v));
+  c.remove("c");
+  ASSERT_EQ(1, c.size());
+  ASSERT_EQ(false, c.find("c", v));
+  c.remove("d");
+  ASSERT_EQ(0, c.size());
+  ASSERT_EQ(false, c.find("d", v));
+}
+
+// Test 9
+// Tests to make sure range can include entire list
+// and include larger numbers
+TEST(BasicListTest, AdvancedRange) {
+  BinSearchCollection<int,string> c;
+  c.add(50, "e");
+  c.add(10, "a");
+  c.add(30, "c");
+  c.add(40, "d");
+  c.add(100, "g");
+  c.add(2000, "h");
+  c.add(60, "f");
+  c.add(20, "b");
+  vector<string> vs;
+  c.find(20, 40, vs);
+  ASSERT_EQ(3, vs.size());
+  vector<string> vs2;
+  c.find(10, 2000, vs2);
+  ASSERT_EQ(8, vs2.size());
+  // note that the following "find" is a C++ built-in function
+  ASSERT_EQ(vs.end(), find(vs.begin(), vs.end(), "a"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "b"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "c"));
+  ASSERT_NE(vs.end(), find(vs.begin(), vs.end(), "d"));
+  ASSERT_EQ(vs.end(), find(vs.begin(), vs.end(), "e"));
+  ASSERT_EQ(vs.end(), find(vs.begin(), vs.end(), "f"));
+}
+
+// Test 10
+// Tests sorting list with larger and negative numbers
+TEST(BasicListTest, AdvancedSort) {
+  BinSearchCollection<string,int> c;
+  c.add("g", 100);
+  c.add("b", 10);
+  c.add("f", 50);
+  c.add("d", 30);
+  c.add("a", -100);
+  c.add("h", 1000);
+  c.add("c", 20);
+  c.add("e", 40);
+  c.add("i", 10000);
+  vector<string> sorted_ks;
+  c.sort(sorted_ks);
+  ASSERT_EQ(9, sorted_ks.size());
+  // check if in sorted order
+  for (int i = 0; i < int(sorted_ks.size()) -1; ++i)
+    ASSERT_LE(sorted_ks[i], sorted_ks[i+1]);
+}
 
 int main(int argc, char** argv)
 {
